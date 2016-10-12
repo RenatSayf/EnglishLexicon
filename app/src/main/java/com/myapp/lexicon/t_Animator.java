ec23521ea1342dfab4ca92b6647f7e4ecdae1c90
renatsayf <renatsayf@gmail.com>
2016-10-12 16:31:25 +0500

    ok

--- app/src/main/java/com/myapp/lexicon/t_Animator.java
+++ app/src/main/java/com/myapp/lexicon/t_Animator.java
@@ -1,7 +1,6 @@
 package com.myapp.lexicon;
 
 import android.animation.Animator;
-import android.view.View;
 import android.view.ViewPropertyAnimator;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
@@ -15,6 +14,8 @@ import android.widget.TextView;
 public class t_Animator extends Animation
 {
     public static ITextViewToleftEnd iTextViewToleftEnd;
+    public static IButtonsToDownEnd iButtonsToDownEnd;
+    public static IButtonToTopListener iButtonToTopListener;
 
     private Button[] buttons;
     private TextView textView;
@@ -38,9 +39,28 @@ public class t_Animator extends Animation
         iTextViewToleftEnd = end;
     }
 
+    public interface IButtonsToDownEnd
+    {
+        void buttonsToDownEnd(int result);
+    }
+
+    public void registrationButtonsToDownEnd(IButtonsToDownEnd end)
+    {
+        iButtonsToDownEnd = end;
+    }
+
+    public interface IButtonToTopListener
+    {
+        void buttonToTopListener(int result);
+    }
+
+    public void setButtonToTopListener(IButtonToTopListener end)
+    {
+        iButtonToTopListener = end;
+    }
+
     public void textViewToLeft()
     {
-
         textView.animate().x(-(textView.getWidth() + delta))
                 .setDuration(duration)
                 .setInterpolator(new AccelerateDecelerateInterpolator())
@@ -83,5 +103,177 @@ public class t_Animator extends Animation
         animToRight = buttons[index].animate().x((buttons[index].getWidth() + delta))
                 .setDuration(duration)
                 .setInterpolator(new AccelerateDecelerateInterpolator());
+    }
+
+    public void buttonsToDown(int index)
+    {
+        for (int i=index-1; i >= 0; i--)
+        {
+            animToDown = buttons[i].animate()
+                    .translationYBy(buttons[index].getHeight())
+                    .setDuration(duration)
+                    .setInterpolator(new AccelerateDecelerateInterpolator());
+            if (i == 0)
+            {
+                buttonsToDown_Listener(animToDown);
+            }
+        }
+    }
+
+    private void buttonsToDown_Listener(ViewPropertyAnimator animToDown)
+    {
+        animToDown.setListener(new Animator.AnimatorListener()
+        {
+            @Override
+            public void onAnimationStart(Animator animation)
+            {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation)
+            {
+                if (iButtonsToDownEnd != null)
+                {
+                    iButtonsToDownEnd.buttonsToDownEnd(1);
+                }
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation)
+            {
+                if (iButtonsToDownEnd != null)
+                {
+                    iButtonsToDownEnd.buttonsToDownEnd(-1);
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation)
+            {
+
+            }
+        });
+    }
+
+    public void buttonToTop(int index)
+    {
+        animToTop = buttons[index].animate()
+                .translationYBy(-buttons[index].getHeight() * index)
+                .setDuration(10)
+                .setListener(new Animator.AnimatorListener()
+                {
+                    @Override
+                    public void onAnimationStart(Animator animation)
+                    {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation)
+                    {
+                        if (iButtonToTopListener != null)
+                        {
+                            iButtonToTopListener.buttonToTopListener(1);
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation)
+                    {
+                        if (iButtonToTopListener != null)
+                        {
+                            iButtonToTopListener.buttonToTopListener(-1);
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation)
+                    {
+
+                    }
+                });
+    }
+
+    public static IButtonToLeftListener iButtonToLeftListener;
+    public void setButtonToLeftListener(IButtonToLeftListener listener)
+    {
+        iButtonToLeftListener = listener;
+    }
+
+    public interface IButtonToLeftListener
+    {
+        void buttonToLeftListener(int result);
+    }
+
+    public void buttonToLeft(int index)
+    {
+        buttons[index].animate().x(delta).setDuration(duration)
+                .setInterpolator(new AccelerateDecelerateInterpolator())
+                .setListener(new Animator.AnimatorListener()
+                {
+                    @Override
+                    public void onAnimationStart(Animator animation)
+                    {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation)
+                    {
+
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation)
+                    {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation)
+                    {
+
+                    }
+                });
+    }
+
+    public void textViewToRight()
+    {
+        textView.animate().translationX(0)
+                .setDuration(duration)
+                .setInterpolator(new AccelerateDecelerateInterpolator())
+                .setListener(new Animator.AnimatorListener()
+                {
+                    @Override
+                    public void onAnimationStart(Animator animation)
+                    {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation)
+                    {
+                        if (iButtonToLeftListener != null)
+                        {
+                            iButtonToLeftListener.buttonToLeftListener(1);
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation)
+                    {
+                        if (iButtonToLeftListener != null)
+                        {
+                            iButtonToLeftListener.buttonToLeftListener(1);
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation)
+                    {
+
+                    }
+                });
     }
 }
--- app/src/main/java/com/myapp/lexicon/t_OneOfFiveTest.java
+++ app/src/main/java/com/myapp/lexicon/t_OneOfFiveTest.java
@@ -22,7 +22,8 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class t_OneOfFiveTest extends Fragment implements t_Animator.ITextViewToleftEnd
+public class t_OneOfFiveTest extends Fragment implements t_Animator.ITextViewToleftEnd,
+        t_Animator.IButtonsToDownEnd, t_Animator.IButtonToTopListener, t_Animator.IButtonToLeftListener
 {
     public static final int ROWS = 5;
     private static Button[] buttonsArray;
@@ -184,29 +185,43 @@ public class t_OneOfFiveTest extends Fragment implements t_Animator.ITextViewTol
                 randomGenerator = new z_RandomNumberGenerator(controlListSize, 133);
                 for (int i = 0; i < controlList.size(); i++)
                 {
+                    Button button = (Button) buttonsLayout.getChildAt(i);
+                    button.setVisibility(View.VISIBLE);
+                    button.setTranslationX(0);
+                    button.setTranslationY(0);
+                    btnLeft_OnClick(button, i);
+                    if (button.getVisibility() == View.VISIBLE)
+                    {
+                        buttonsArray[i] = button;
+                    }
+
                     buttonsArray[i].setText(controlList.get(i).get_translate());
                     wordIndex++;
                 }
                 int randIndex = randomGenerator.generate();
                 textView.setText(list.get(randIndex).get_english());
+
                 animator = new t_Animator(buttonsArray, textView);
                 animator.registrationITextViewToleftEnd(t_OneOfFiveTest.this);
+                animator.registrationButtonsToDownEnd(t_OneOfFiveTest.this);
+                animator.setButtonToTopListener(t_OneOfFiveTest.this);
+                animator.setButtonToLeftListener(t_OneOfFiveTest.this);
             }
         };
         asyncTask.execute(spinnSelectedItem, wordIndex, count);
         asyncTask = null;
 
-        for (int i = 0; i < count; i++)
-        {
-            Button button = (Button) buttonsLayout.getChildAt(i);
-            button.setVisibility(View.VISIBLE);
-            button.setTranslationX(0);
-            btnLeft_OnClick(button, i);
-            if (button.getVisibility() == View.VISIBLE)
-            {
-                buttonsArray[i] = button;
-            }
-        }
+//        for (int i = 0; i < count; i++)
+//        {
+//            Button button = (Button) buttonsLayout.getChildAt(i);
+//            button.setVisibility(View.VISIBLE);
+//            button.setTranslationX(0);
+//            btnLeft_OnClick(button, i);
+//            if (button.getVisibility() == View.VISIBLE)
+//            {
+//                buttonsArray[i] = button;
+//            }
+//        }
     }
     private static int controlListSize = 0;
     private void btnLeft_OnClick(final View view, final int index)
@@ -288,7 +303,6 @@ public class t_OneOfFiveTest extends Fragment implements t_Animator.ITextViewTol
             }
             textView.setText(controlList.get(randomNumber).get_english());
 
-
         }
         else if (listFromDB.size() == 0 && controlList.size() <= ROWS)
         {
@@ -300,14 +314,76 @@ public class t_OneOfFiveTest extends Fragment implements t_Animator.ITextViewTol
                 int randomNumber = randomGenerator.generate();
                 textView.setText(controlList.get(randomNumber).get_english());
             }
-            buttonsArray[btn_position].setVisibility(View.GONE);
+            buttonsArray[btn_position].setVisibility(View.INVISIBLE);
         }
+        if (btn_position == 0)
+        {
+            animator.buttonToRight(btn_position);
+        }
+
+        if (btn_position > 0)
+        {
+            animator.buttonsToDown(btn_position);
+        }
+
+
         wordIndex++;
     }
 
-    private void getNextWordFromDB(String tableName, int index)
+    @Override
+    public void buttonsToDownEnd(int result)
     {
+        //Toast.makeText(getActivity(), "buttonsToDownEnd = "+result, Toast.LENGTH_SHORT).show();
+        //animator.textViewToRight();
+        animator.buttonToTop(btn_position);
+    }
 
+    @Override
+    public void buttonToTopListener(int result)
+    {
+        animator.textViewToRight();
+        animator.buttonToLeft(btn_position);
+    }
+
+    @Override
+    public void buttonToLeftListener(int result)
+    {
+        Button[] newArray = null;
+        if (true)
+        {
+            int j = 0;
+            newArray = new Button[buttonsArray.length];
+            newArray[0] = buttonsArray[btn_position];
+            for (int i=0; i < btn_position; i++)
+            {
+                j++;
+                newArray[j] = buttonsArray[i];
+            }
+
+            for (int i = btn_position+1; i < buttonsArray.length; i++)
+            {
+                newArray[i] = buttonsArray[i];
+
+            }
+            //updateArrayClickListenerLeft(newArray);
+        } else
+        {
+
+        }
+    }
+
+    private void updateArrayClickListenerLeft(Button[] newArray)
+    {
+        buttonsArray = null;
+        buttonsArray = newArray;
+        for (int i = 0; i < buttonsArray.length; i++)
+        {
+            if (buttonsArray[i] != null)
+            {
+                btnLeft_OnClick(buttonsArray[i], i);
+            }
+        }
+        Button[] arrayBtn = buttonsArray;
     }
 
 
