cd33fc49970fb8e77a3bfe03c30e275950c94cf9
renatsayf <renatsayf@gmail.com>
2016-12-06 16:35:11 +0500

    startTest почти готов

--- app/src/main/java/com/myapp/lexicon/t_FindPairFragment.java
+++ app/src/main/java/com/myapp/lexicon/t_FindPairFragment.java
@@ -2,16 +2,28 @@ package com.myapp.lexicon;
 
 
 import android.animation.Animator;
+import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -19,8 +31,9 @@ import android.widget.RelativeLayout;
  */
 public class t_FindPairFragment extends Fragment
 {
+    public static int ROWS = 5;
+
     private static RelativeLayout.LayoutParams saveTopPanelParams;
-    private static boolean isOpen = false;
 
     private LinearLayout linLayout;
     private LinearLayout topPanel;
@@ -28,7 +41,41 @@ public class t_FindPairFragment extends Fragment
     private float touchDown = 0, touchUp = 0;
     private Button topPanelButtonOK;
     private Button topPanelButtonFinish;
+    private TextView headerTopPanel;
     private long duration = 1000;
+    private static boolean isOpen = false;
+
+    private LinearLayout btnLayoutLeft, btnLayoutRight;
+    private Button buttonWord;
+
+    private Spinner spinnListDict;
+    private int spinnSelectedIndex = -1;
+    private String spinnSelectedItem;
+    private static ArrayList<String> storedListDict = new ArrayList<>();
+
+    private ProgressBar progressBar;
+
+    private z_LockOrientation lockOrientation;
+    private int wordIndex = 1;
+    private int wordsCount;
+    private static int counterRightAnswer = 0;
+    private static ArrayList<String> textArrayleft = new ArrayList<>();
+    private static ArrayList<String> textArrayRight = new ArrayList<>();
+    private static ArrayList<DataBaseEntry> controlList;
+    private static ArrayList<DataBaseEntry> additionalList;
+    private static int additonalCount = 0;
+    private int controlListSize = 0;
+    private static z_RandomNumberGenerator randomGenerator;
+    private int range = 4;
+    DisplayMetrics metrics;
+
+    private String KEY_CONTROL_LIST_SIZE = "key_control_list_size";
+    private String KEY_WORDS_COUNT = "key_words_count";
+    private String KEY_SPINN_SELECT_ITEM = "key_spinn_select_item";
+    private String KEY_SPINN_SELECT_INDEX = "key_spinn_select_index";
+    private String KEY_PROGRESS = "key_progress";
+    private String KEY_PROGRESS_MAX = "key_progress_max";
+    private String KEY_WORD_INDEX = "key_word_index";
 
 
     public t_FindPairFragment()
@@ -39,6 +86,14 @@ public class t_FindPairFragment extends Fragment
     @Override
     public void onSaveInstanceState(Bundle outState)
     {
+        outState.putString(KEY_SPINN_SELECT_ITEM, spinnSelectedItem);
+        outState.putInt(KEY_SPINN_SELECT_INDEX, spinnSelectedIndex);
+        outState.putInt(KEY_PROGRESS, progressBar.getProgress());
+        outState.putInt(KEY_PROGRESS_MAX, progressBar.getMax());
+        outState.putInt(KEY_WORD_INDEX, wordIndex);
+        outState.putInt(KEY_WORDS_COUNT, wordsCount);
+        outState.putInt(KEY_CONTROL_LIST_SIZE, controlListSize);
+
         if (isOpen)
         {
             saveTopPanelParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
@@ -52,10 +107,17 @@ public class t_FindPairFragment extends Fragment
                              Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
+        lockOrientation = new z_LockOrientation(getActivity());
+        Display display = getActivity().getWindowManager().getDefaultDisplay();
+        metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+
         View fragment_view = inflater.inflate(R.layout.t_find_pair_fragment, container, false);
         topPanel = (LinearLayout) fragment_view.findViewById(R.id.top_panel);
         topPanelParams = (RelativeLayout.LayoutParams) topPanel.getLayoutParams();
         linLayout = (LinearLayout) fragment_view.findViewById(R.id.lin_layout_find_pair);
+        headerTopPanel = (TextView) fragment_view.findViewById(R.id.header_top_panel);
+        headerTopPanel.setText("Найди парные слова");
         topPanelButtonOK = (Button) fragment_view.findViewById(R.id.btn_ok);
         topPanelButtonFinish = (Button) fragment_view.findViewById(R.id.btn_complete);
         topPanelButtons_OnClick();
@@ -78,11 +140,161 @@ public class t_FindPairFragment extends Fragment
                 return true;
             }
         });
+        if (savedInstanceState != null && isOpen)
+        {
+            topPanel.setLayoutParams(saveTopPanelParams);
+        }
+
+        spinnListDict = (Spinner) fragment_view.findViewById(R.id.spinner_dict);
+        spinnListDict_OnItemSelectedListener();
+        setItemsToSpinnListDict();
+        if (savedInstanceState != null)
+        {
+            spinnSelectedItem = savedInstanceState.getString(KEY_SPINN_SELECT_ITEM);
+            spinnSelectedIndex = savedInstanceState.getInt(KEY_SPINN_SELECT_INDEX);
+        }
+
+        progressBar = (ProgressBar) fragment_view.findViewById(R.id.prog_bar_find_pair);
+        if (savedInstanceState != null)
+        {
+            progressBar.setProgress(savedInstanceState.getInt(KEY_PROGRESS));
+            progressBar.setMax(savedInstanceState.getInt(KEY_PROGRESS_MAX));
+        }
+
+        if (savedInstanceState != null)
+        {
+            wordIndex = savedInstanceState.getInt(KEY_WORD_INDEX);
+            wordsCount = savedInstanceState.getInt(KEY_WORDS_COUNT);
+            controlListSize = savedInstanceState.getInt(KEY_CONTROL_LIST_SIZE);
+        }
+
+        btnLayoutLeft = (LinearLayout) fragment_view.findViewById(R.id.btn_layout_left);
+        btnLayoutRight = (LinearLayout) fragment_view.findViewById(R.id.btn_layout_right);
+        if (savedInstanceState == null)
+        {
+            hideWordButtons();
+        }
 
         return fragment_view;
     }
 
+    private void spinnListDict_OnItemSelectedListener()
+    {
+        spinnListDict.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, final int position, long id)
+            {
+                if (position == spinnSelectedIndex) return;
+//                textArray.clear();
+                startTest(position);
+                topPanelVisible(1, 0, isOpen);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+
+            }
+        });
+    }
+
+    private void setItemsToSpinnListDict()
+    {
+        if (storedListDict.size() > 0)
+        {
+            ArrayAdapter<String> spinnAdapter = new ArrayAdapter<>(getActivity().getApplicationContext(), R.layout.my_content_spinner_layout, storedListDict);
+            spinnListDict.setAdapter(spinnAdapter);
+            return;
+        }
+
+        new DataBaseQueries.GetLictTableAsync()
+        {
+            @Override
+            public void resultAsyncTask(ArrayList<String> list)
+            {
+                ArrayAdapter<String> adapterSpinner= new ArrayAdapter<>(getActivity().getApplicationContext(), R.layout.my_content_spinner_layout, list);
+                spinnListDict.setAdapter(adapterSpinner);
+                spinnListDict.setSelection(spinnSelectedIndex);
+                spinnListDict_OnItemSelectedListener();
+                for (int i = 0; i < spinnListDict.getAdapter().getCount(); i++)
+                {
+                    storedListDict.add(spinnListDict.getAdapter().getItem(i).toString());
+                }
+            }
+        }.execute();
+    }
+
+    private void startTest(final int position)
+    {
+        lockOrientation.lock();
+        wordIndex = 1;
+        spinnSelectedItem = spinnListDict.getSelectedItem().toString();
+        DataBaseQueries.GetWordsCountAsync getWordsCount = new DataBaseQueries.GetWordsCountAsync()
+        {
+            @Override
+            public void resultAsyncTask(int res)
+            {
+                wordsCount = res;
+                spinnSelectedIndex = position;
+                progressBar.setMax(res);
+                progressBar.setProgress(0);
+                counterRightAnswer = 0;
+                topPanelVisible(0, 1, isOpen);
+                //hideWordButtons();
+
+                AsyncTask<Object, Void, ArrayList<DataBaseEntry>> asyncTask = new DataBaseQueries.GetWordsFromDBAsync()
+                {
+                    @Override
+                    public void resultAsyncTask(ArrayList<DataBaseEntry> list)
+                    {
+                        controlList = list;
+                        additionalList = new ArrayList<>();
+                        additonalCount = 0;
+                        for (DataBaseEntry entry : list)
+                        {
+                            additionalList.add(entry);
+                        }
+                        controlListSize = controlList.size();
+
+                        for (int i = 0; i < controlList.size(); i++)
+                        {
+                            Button btnLeft = (Button) btnLayoutLeft.getChildAt(i);
+                            Button btnRight = (Button) btnLayoutRight.getChildAt(i);
+                            btnLeft.setText(controlList.get(i).get_english());
+                            btnRight.setText(controlList.get(i).get_translate());
+                            btnLeft.setX(metrics.widthPixels);
+                            btnRight.setX(-metrics.widthPixels);
+                            btnLeft.setVisibility(View.VISIBLE);
+                            btnRight.setVisibility(View.VISIBLE);
+                            btnLeft.animate().translationX(0).setDuration(duration).setInterpolator(new AnticipateOvershootInterpolator());
+                            btnRight.animate().translationX(0).setDuration(duration).setInterpolator(new AnticipateOvershootInterpolator());
+                            //btnLeft_OnClick(btnLeft);
+                            wordIndex++;
+                        }
+                        randomGenerator = new z_RandomNumberGenerator(controlListSize, range);
+                        int randIndex = randomGenerator.generate();
+
+                        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+                    }
+                };
+                asyncTask.execute(spinnSelectedItem, wordIndex, ROWS);
+                asyncTask = null;
+            }
+        };
+        getWordsCount.execute(spinnSelectedItem);
+    }
+
+    private void hideWordButtons()
+    {
+        for (int i = 0; i < btnLayoutLeft.getChildCount(); i++)
+        {
+            Button btnLeft = (Button) btnLayoutLeft.getChildAt(i);
+            Button btnRight = (Button) btnLayoutRight.getChildAt(i);
+            btnLeft.setVisibility(View.GONE);
+            btnRight.setVisibility(View.GONE);
+        }
+    }
 
     private void topPanelVisible(float touchDown, float touchUp, boolean isOpen)
     {
@@ -161,8 +373,8 @@ public class t_FindPairFragment extends Fragment
             public void onClick(View v)
             {
                 topPanelVisible(1, 0, isOpen);
-                //spinnListDict.setSelection(-1);
-                //spinnSelectedIndex = -1;
+                spinnListDict.setSelection(-1);
+                spinnSelectedIndex = -1;
                 getActivity().onBackPressed();
             }
         });
