24b6b30d014473466676e05e98ee70cdc6d7fe7d
RenatSayf <renatsayf@gmail.com>
2017-04-22 22:56:10 +0500

    Tests.java - was added the banner

---
+++ app/src/main/java/com/myapp/lexicon/wordstests/BannerFragmentTests.java
@@ -0,0 +1,67 @@
+package com.myapp.lexicon.wordstests;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.myapp.lexicon.R;
+import com.myapp.lexicon.settings.AppData;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BannerFragmentTests extends Fragment
+{
+    private View fragmentView = null;
+
+    public BannerFragmentTests()
+    {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        if (fragmentView == null)
+        {
+            fragmentView = inflater.inflate(R.layout.t_banner_fragment_tests, container, false);
+        }
+
+        if (savedInstanceState == null)
+        {
+            final AdView bannerView = (AdView) fragmentView.findViewById(R.id.adView_tests);
+            bannerView.setVisibility(View.GONE);
+            MobileAds.initialize(getActivity(), getString(R.string.play_list_banner));
+            //AdRequest adRequest = new AdRequest.Builder().build();
+            AdRequest adRequest = new AdRequest.Builder().addTestDevice(AppData.getInstance().getTestDeviceID()).build();
+            bannerView.loadAd(adRequest);
+            bannerView.setAdListener(new AdListener()
+            {
+                @Override
+                public void onAdLoaded()
+                {
+                    super.onAdLoaded();
+                    bannerView.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+
+        return fragmentView;
+    }
+
+}
