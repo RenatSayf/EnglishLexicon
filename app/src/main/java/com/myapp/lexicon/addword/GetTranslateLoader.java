1fa38ee351344e56f5aba10286f3a4b0f768cbe9
RenatSayf <renatsayf@gmail.com>
2017-04-23 17:36:31 +0500

    GetTranslateLoader.java was modified, API-keys was added

--- app/src/main/java/com/myapp/lexicon/addword/GetTranslateLoader.java
+++ app/src/main/java/com/myapp/lexicon/addword/GetTranslateLoader.java
@@ -29,7 +29,6 @@ public class GetTranslateLoader extends AsyncTaskLoader
 {
     public static final String KEY_TEXT_ENTERED = "text_entered";
 
-    //private Context context;
     private String langSystem;
     private String textEntered;
     private DataBaseQueries dataBaseQueries;
@@ -37,14 +36,17 @@ public class GetTranslateLoader extends AsyncTaskLoader
             {
                     "trnsl.1.1.20160128T151725Z.d4bbd1b06137bfde.2e1323688363a820f730629556d68f2b9f0a1a19",
                     "trnsl.1.1.20160607T122324Z.85a9ab6b9e12baac.f5b66628231eb6175c5cf1393d1601c2cfb5d553",
-                    "trnsl.1.1.20170126T141135Z.59c382f4c439bbaf.3226485ac0b3691894625bddb3c48c74f5067706"
+                    "trnsl.1.1.20170126T141135Z.59c382f4c439bbaf.3226485ac0b3691894625bddb3c48c74f5067706",
+                    "trnsl.1.1.20170423T121414Z.3240e7971183355e.e2d03076a138b1ee0324ae8e6db4aa072ddc3390",
+                    "trnsl.1.1.20170423T121504Z.806c486ec21aa245.7d9eba215a558252d45d446cf7c9ace081ee2a97",
+                    "trnsl.1.1.20170423T121559Z.11399a044f857e17.8a31fb51a77ca0877235350e67334b587a5eee75",
+                    "trnsl.1.1.20170423T121720Z.23f1ac5940850aeb.41f79bbea45ddc51abaf7482e12584fba14798b4"
             };
     private Random generator;
 
     public GetTranslateLoader(Context context, Bundle bundle)
     {
         super(context);
-        //this.context = context;
         if (bundle != null)
         {
             textEntered = bundle.getString(KEY_TEXT_ENTERED);
@@ -80,7 +82,6 @@ public class GetTranslateLoader extends AsyncTaskLoader
         }
 
         String format = "plain";
-        //String keyApi = "trnsl.1.1.20160607T122324Z.85a9ab6b9e12baac.f5b66628231eb6175c5cf1393d1601c2cfb5d553";
         String apiKey = dataBaseQueries.getApiKey();
         if (apiKey.equals(""))
         {
