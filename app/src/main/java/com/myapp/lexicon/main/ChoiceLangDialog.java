299e650cc1c73a256d07e4d10cab3ac44de769e0
RenatSayf <renatsayf@gmail.com>
2018-03-08 11:53:17 +0500

    a_lang_layout.xml was removed, a_choice_lang_dialog.xml was modified

--- app/src/main/java/com/myapp/lexicon/main/ChoiceLangDialog.java
+++ app/src/main/java/com/myapp/lexicon/main/ChoiceLangDialog.java
@@ -6,7 +6,6 @@ import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;
-import android.widget.ListView;
 
 import com.myapp.lexicon.R;
 import com.myapp.lexicon.settings.AppSettings;
@@ -19,21 +18,16 @@ public class ChoiceLangDialog extends DialogFragment
 {
     public static final String TAG = "choice_lang_dialog";
 
-    private LangListViewAdapter listViewAdapter;
     private AppSettings appSettings;
-    private ListView langListView;
+
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState)
     {
         View dialogView = getActivity().getLayoutInflater().inflate(R.layout.a_choice_lang_dialog, null);
-        langListView = dialogView.findViewById(R.id.lang_list_view);
 
         appSettings = new AppSettings(getActivity());
-        listViewAdapter = new LangListViewAdapter(appSettings.getTransLangList(), getActivity());
-        langListView.setAdapter(listViewAdapter);
-
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
                 .setTitle("Choose Your language")
