90855f926e32126db08e21dfcbe3b743cd2afb03
RenatSayf <renatsayf@gmail.com>
2018-04-13 23:27:32 +0500

    FindPairFragment.java was fixed

--- app/src/main/java/com/myapp/lexicon/wordstests/FindPairFragment.java
+++ app/src/main/java/com/myapp/lexicon/wordstests/FindPairFragment.java
@@ -23,7 +23,6 @@ import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -587,14 +586,20 @@ public class FindPairFragment extends Fragment implements DialogTestComplete.IDi
             tempButtonRight.setBackgroundResource(R.drawable.text_btn_for_test_green);
             HashMap<String, String> hashMap = new HashMap<>();
             hashMap.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "find_pair_fragm");
-            SplashScreenActivity.speech.setLanguage(Locale.US);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            try
             {
-                SplashScreenActivity.speech.speak(enword, TextToSpeech.QUEUE_ADD, null, hashMap.get(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID));
-            }
-            else
+                SplashScreenActivity.speech.setLanguage(Locale.US);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                {
+                    SplashScreenActivity.speech.speak(enword, TextToSpeech.QUEUE_ADD, null, hashMap.get(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID));
+                }
+                else
+                {
+                    SplashScreenActivity.speech.speak(enword, TextToSpeech.QUEUE_ADD, hashMap);
+                }
+            } catch (Exception e)
             {
-                SplashScreenActivity.speech.speak(enword, TextToSpeech.QUEUE_ADD, hashMap);
+                e.printStackTrace();
             }
 
             ViewPropertyAnimator animScale = tempButtonLeft.animate().scaleX(0).scaleY(0).setDuration(duration).setInterpolator(new AnticipateOvershootInterpolator()).setStartDelay(0);
