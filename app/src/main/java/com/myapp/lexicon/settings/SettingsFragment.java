24a140d7f4d394942694fcc4413681780c3a1cdc
RenatSayf <renatsayf@gmail.com>
2018-04-15 22:01:27 +0500

    pref.xml and SettingsFragment.java were modified

--- app/src/main/java/com/myapp/lexicon/settings/SettingsFragment.java
+++ app/src/main/java/com/myapp/lexicon/settings/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.myapp.lexicon.settings;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -13,12 +15,27 @@ import com.myapp.lexicon.R;
 
 public class SettingsFragment extends PreferenceFragment
 {
+    ListPreference listDisplayModePref;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref);
+        listDisplayModePref = (ListPreference) findPreference("list_display_mode");
+        // при новом создании экрана заполняем summary значением настройки
+        listDisplayModePref.setSummary(listDisplayModePref.getEntry());
 
+        listDisplayModePref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener()
+        {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue)
+            {
+                listDisplayModePref.setValue(newValue.toString());
+                listDisplayModePref.setSummary(listDisplayModePref.getEntry());
+                return true;
+            }
+        });
     }
 
     @Override
