d2b9074237db7e3c71d66b7a2baa6483dfc91975
RenatSayf <renatsayf@gmail.com>
2018-08-18 18:37:55 +0500

    Share.java was fixed

--- app/src/main/java/com/myapp/lexicon/helpers/Share.java
+++ app/src/main/java/com/myapp/lexicon/helpers/Share.java
@@ -9,12 +9,15 @@ public class Share
 {
     public void doShare(Context context)
     {
-        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-        sharingIntent.setType("text/plain");
-        String shareBody = "http://play.google.com/store/apps/details?id=" + context.getPackageName();
-        String shareSub = context.getString(R.string.text_excellent_app);
-        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
-        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-        context.startActivity(Intent.createChooser(sharingIntent, context.getString(R.string.text_share_using)));
+        if (context != null)
+        {
+            Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            String shareBody = "http://play.google.com/store/apps/details?id=" + context.getPackageName();
+            String shareSub = context.getString(R.string.text_excellent_app);
+            sharingIntent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
+            sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+            context.startActivity(Intent.createChooser(sharingIntent, context.getString(R.string.text_share_using)));
+        }
     }
 }
