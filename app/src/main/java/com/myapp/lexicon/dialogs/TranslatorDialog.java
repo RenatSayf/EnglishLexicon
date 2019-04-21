6b7cbbb910d15057a75faed9bc9cb580c7e7521b
Ренат Сайфутдинов <renatsayf@gmail.com>
2019-04-21 14:40:57 +0500

    TranslateApi.java has been modified

--- app/src/main/java/com/myapp/lexicon/dialogs/TranslatorDialog.java
+++ app/src/main/java/com/myapp/lexicon/dialogs/TranslatorDialog.java
@@ -9,15 +9,20 @@ import android.support.v7.app.AppCompatDialogFragment;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.myapp.lexicon.R;
 import com.myapp.lexicon.connectivity.TranslateApi;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class TranslatorDialog extends AppCompatDialogFragment
 {
@@ -25,7 +30,8 @@ public class TranslatorDialog extends AppCompatDialogFragment
     public static final String EN_WORD_TAG = "en_word";
     private static final String RU_WORD_TAG = "ru_word";
 
-    private static TranslatorDialog dialog = null;
+    private EditText editTextRu;
+    private ProgressBar progressBar;
 
     public TranslatorDialog()
     {
@@ -34,10 +40,7 @@ public class TranslatorDialog extends AppCompatDialogFragment
 
     public static TranslatorDialog getInstance(String enWord)
     {
-        if (dialog == null)
-        {
-            dialog = new TranslatorDialog();
-        }
+        TranslatorDialog dialog = new TranslatorDialog();
         Bundle bundle = new Bundle();
         bundle.putString(EN_WORD_TAG, enWord);
         dialog.setArguments(bundle);
@@ -48,32 +51,7 @@ public class TranslatorDialog extends AppCompatDialogFragment
     public void onCreate(final Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null && getActivity() != null)
-        {
-            RequestQueue queue = Volley.newRequestQueue(getActivity());
-            String url = new TranslateApi(getActivity()).getStringUrl(getArguments().getString(EN_WORD_TAG));
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>()
-            {
-                @Override
-                public void onResponse(String response)
-                {
-                    if (getArguments() != null)
-                    {
-                        getArguments().putString(RU_WORD_TAG, null);
-                    }
-                    return;
-                }
-            },
-                    new Response.ErrorListener()
-                    {
-                        @Override
-                        public void onErrorResponse(VolleyError error)
-                        {
-                            return;
-                        }
-                    });
-            queue.add(stringRequest);
-        }
+
     }
 
     @NonNull
@@ -84,6 +62,8 @@ public class TranslatorDialog extends AppCompatDialogFragment
         {
             View dialogView = getActivity().getLayoutInflater().inflate(R.layout.dialog_translator, new LinearLayout(getContext()), false);
             EditText editTextEn = dialogView.findViewById(R.id.en_word_et);
+            editTextRu = dialogView.findViewById(R.id.ru_word_et);
+            progressBar = dialogView.findViewById(R.id.prog_bar_dialog_trans);
             if (getArguments() != null)
             {
                 editTextEn.setText(getArguments().getString(EN_WORD_TAG));
@@ -111,8 +91,53 @@ public class TranslatorDialog extends AppCompatDialogFragment
         {
             return super.onCreateDialog(savedInstanceState);
         }
-
     }
 
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        if (getArguments() != null && getActivity() != null)
+        {
+            RequestQueue queue = Volley.newRequestQueue(getActivity());
+            String url = new TranslateApi(getActivity()).getStringUrl(getArguments().getString(EN_WORD_TAG));
+            JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>()
+            {
+                @Override
+                public void onResponse(JSONObject response)
+                {
+                    progressBar.setVisibility(View.GONE);
+                    try
+                    {
+                        JSONArray array = response.getJSONArray("text");
+                        StringBuilder ruText = new StringBuilder();
+                        for (int i = 0; i < array.length(); i++)
+                        {
+                            ruText.append(array.get(i)).append(", ");
+                        }
 
+                        editTextRu.setText(ruText.toString().replaceAll("/(,\\s)$/g", ""));
+                    } catch (JSONException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    return;
+                }
+
+
+            },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error)
+                        {
+                            progressBar.setVisibility(View.GONE);
+                            return;
+                        }
+                    });
+            queue.add(jsonRequest);
+            progressBar.animate();
+        }
+
+    }
 }
--- app/src/main/java/com/myapp/lexicon/main/BackgroundFragm.java
+++ app/src/main/java/com/myapp/lexicon/main/BackgroundFragm.java
@@ -99,4 +99,5 @@ public class BackgroundFragm extends Fragment
         return fragmentView;
     }
 
+
 }
