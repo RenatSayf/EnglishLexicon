2a8e5e01753be0d1abcf60127f15e3f0802f9600
renatsayf <renatsayf@gmail.com>
2020-08-19 00:02:52 +0500

    TranslateActivity.java has been updated

--- app/src/main/java/com/myapp/lexicon/addword/TranslateActivity.java
+++ app/src/main/java/com/myapp/lexicon/addword/TranslateActivity.java
@@ -4,13 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.myapp.lexicon.R;
-import com.myapp.lexicon.dialogs.TranslatorDialog;
 
 import java.util.Objects;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDialogFragment;
-import androidx.fragment.app.FragmentManager;
 
 public class TranslateActivity extends AppCompatActivity
 {
@@ -29,22 +26,24 @@ public class TranslateActivity extends AppCompatActivity
                 if (sequence != null)
                 {
                     enWord = Objects.requireNonNull(intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)).toString().toLowerCase();
-                    TranslatorDialog translatorDialog = TranslatorDialog.getInstance(enWord, new TranslatorDialog.NoticeDialogListener()
-                    {
-                        @Override
-                        public void onDialogAddClick(AppCompatDialogFragment dialog)
-                        {
-                            finish();
-                        }
-
-                        @Override
-                        public void onDialogCancelClick(AppCompatDialogFragment dialog)
-                        {
-                            finish();
-                        }
-                    });
-                    FragmentManager fragmentManager = getSupportFragmentManager();
-                    translatorDialog.show(fragmentManager,TranslatorDialog.TAG);
+                    TranslateFragment translateFragment = TranslateFragment.Companion.getInstance(enWord);
+                    getSupportFragmentManager().beginTransaction().add(R.id.translate_fragment, translateFragment).addToBackStack(null).commit();
+//                    TranslatorDialog translatorDialog = TranslatorDialog.getInstance(enWord, new TranslatorDialog.NoticeDialogListener()
+//                    {
+//                        @Override
+//                        public void onDialogAddClick(AppCompatDialogFragment dialog)
+//                        {
+//                            finish();
+//                        }
+//
+//                        @Override
+//                        public void onDialogCancelClick(AppCompatDialogFragment dialog)
+//                        {
+//                            finish();
+//                        }
+//                    });
+//                    FragmentManager fragmentManager = getSupportFragmentManager();
+//                    translatorDialog.show(fragmentManager,TranslatorDialog.TAG);
                 }
             }
         }
