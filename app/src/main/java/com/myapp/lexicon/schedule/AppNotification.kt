0426cdda452af0992fc55071b001b8e0b50b3587
renatsayf <renatsayf@gmail.com>
2020-11-29 23:14:02 +0500

    AppNotification.kt has been updated

--- app/src/main/java/com/myapp/lexicon/schedule/AppNotification.kt
+++ app/src/main/java/com/myapp/lexicon/schedule/AppNotification.kt
@@ -6,8 +6,10 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import androidx.preference.PreferenceManager
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
 import com.google.gson.reflect.TypeToken
@@ -24,12 +26,14 @@ class AppNotification constructor(private val context: Context) : Notification()
         const val CHANEL_ID : String = "${NOTIFICATION_ID}.service_notification"
     }
 
+    private var preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+    private lateinit var notification: Notification
+    private var displayMode: String = "0"
+
     init
     {
-
+        displayMode = preferences.getString(context.getString(R.string.key_list_display_mode), "0").toString()
     }
-
-    private lateinit var notification: Notification
 
     fun create(json: String) : Notification
     {
@@ -55,20 +59,22 @@ class AppNotification constructor(private val context: Context) : Notification()
         }
 
         val pendingIntent = PendingIntent.getActivity(context, 0, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-        notification = Notification()
-        notification = NotificationCompat.Builder(context, CHANEL_ID)
-                .setOngoing(true)
-                .setSmallIcon(R.drawable.ic_lexicon_notify)
-                .setContentTitle(pair?.second?.get(0)?.english ?: "JSON ERROR!!!!!!!!!!!!!!!!!!")
-                .setContentText(pair?.second?.get(0)?.translate ?: "JSON ERROR!!!!!!!!!!!!!!!!!!")
-                .setStyle(NotificationCompat.BigTextStyle().bigText(pair?.second?.get(0)?.translate ?: "JSON ERROR!!!!!!!!!!!!!!!!!!"))
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
-                .setPriority(NotificationCompat.PRIORITY_MAX)
-                .setChannelId(CHANEL_ID)
-                .setDefaults(DEFAULT_SOUND)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .build()
+        notification = NotificationCompat.Builder(context, CHANEL_ID).apply {
+            setOngoing(true)
+            setSmallIcon(R.drawable.ic_lexicon_notify)
+            setContentIntent(pendingIntent)
+            setAutoCancel(true)
+            priority = NotificationCompat.PRIORITY_MAX
+            setChannelId(CHANEL_ID)
+            setDefaults(DEFAULT_SOUND)
+            setContentTitle(pair?.second?.get(0)?.english ?: "JSON ERROR!!!!!!!!!!!!!!!!!!")
+            if (displayMode == "1")
+            {
+                setContentText("?????????????")
+            }
+            else setContentText(pair?.second?.get(0)?.translate ?: "JSON ERROR!!!!!!!!!!!!!!!!!!")
+
+        }.build()
 
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
