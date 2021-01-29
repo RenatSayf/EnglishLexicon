3f060b60a3bc6790bc5a9ddcc4e1aeb37bc2f08d
renatsayf <renatsayf@gmail.com>
2021-01-29 23:26:21 +0500

    SQL_Update_in_Words.sql has been updated

--- app/src/main/assets/SQL_Update_in_Words.sql
+++ app/src/main/assets/SQL_Update_in_Words.sql
@@ -1,1 +1,2 @@
-UPDATE Words SET dict_name = 'Наречия' WHERE dict_name == 'Общий'
+--UPDATE Words SET dict_name = 'Наречия' WHERE dict_name == 'Общий'
+UPDATE Words SET count_repeat = 1 WHERE dict_name == 'Наречия'
