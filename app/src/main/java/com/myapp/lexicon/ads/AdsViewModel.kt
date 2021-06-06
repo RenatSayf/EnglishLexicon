76763bd8659fe8f6d1ebfdb246bc8d1c56f23fb6
renatsayf <renatsayf@gmail.com>
2021-06-06 13:27:31 +0500

    MainActivity.java main banner has been added

--- app/src/main/java/com/myapp/lexicon/ads/AdsViewModel.kt
+++ app/src/main/java/com/myapp/lexicon/ads/AdsViewModel.kt
@@ -1,16 +1,11 @@
 package com.myapp.lexicon.ads
 
 import android.app.Application
-import android.content.Context
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 import com.myapp.lexicon.BuildConfig
 import com.myapp.lexicon.R
-import com.myapp.lexicon.billing.KEY_BILLING
-import com.myapp.lexicon.billing.KEY_PURCHASE_TOKEN
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
