4889635733c773c7421dc6070bf64ab9d985fdf7
renatsayf <renatsayf@gmail.com>
2021-08-03 21:55:47 +0500

    MainActivity.java has been refactoring

--- app/src/main/java/com/myapp/lexicon/main/MainActivity.java
+++ app/src/main/java/com/myapp/lexicon/main/MainActivity.java
@@ -692,7 +692,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         else if (id == R.id.nav_check_your_self)
         {
-            getSupportFragmentManager().beginTransaction().replace(R.id.frame_to_page_fragm, TestFragment.Companion.getInstance()).addToBackStack(null).commit();
+            TestFragment testFragment = TestFragment.Companion.getInstance();
+            if (testFragment != null)
+            {
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_to_page_fragm, testFragment).addToBackStack(null).commit();
+            }
         }
         else if (id == R.id.nav_settings)
         {
