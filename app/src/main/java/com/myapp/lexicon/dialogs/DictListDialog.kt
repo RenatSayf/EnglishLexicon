6f25428507509ce88574e3970a179d737040575f
Renat <renatsayf@gmail.com>
2023-04-07 12:11:19 +0500

    DictListDialog.kt has been updated

--- app/src/main/java/com/myapp/lexicon/dialogs/DictListDialog.kt
+++ app/src/main/java/com/myapp/lexicon/dialogs/DictListDialog.kt
@@ -4,15 +4,13 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModelProvider
 import com.myapp.lexicon.R
-import com.myapp.lexicon.main.MainViewModel
+import com.myapp.lexicon.databinding.TitleAlertDialogBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -48,8 +46,17 @@ class DictListDialog : DialogFragment()
     @Suppress("ObjectLiteralToLambda")
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog
     {
-        val builder = AlertDialog.Builder(requireContext()).apply {
+
+        val titleBinding = TitleAlertDialogBinding.inflate(layoutInflater, ConstraintLayout(requireContext()), false)
+        with(titleBinding) {
+
+            tvTitle.text = getString(R.string.text_select_dict)
+            ivIconTitle.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_translate))
+        }
+
+        val builder = AlertDialog.Builder(requireContext(), R.style.AppAlertDialog).apply {
             val stringArray = list.toTypedArray()
+            setCustomTitle(titleBinding.root)
             setSingleChoiceItems(stringArray, 0, object : DialogInterface.OnClickListener
             {
                 override fun onClick(p0: DialogInterface?, position: Int)
@@ -59,10 +66,14 @@ class DictListDialog : DialogFragment()
                     dismiss()
                 }
             })
-            setTitle(getString(R.string.text_dictionaries))
-            setCancelable(true)
-            isCancelable = true
+            setNegativeButton(R.string.text_cancel, object : DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    dismiss()
+                }
+            })
         }
-        return builder.create()
+        return builder.create().apply {
+            this.window?.setBackgroundDrawableResource(R.drawable.bg_popup_dialog)
+        }
     }
 }
