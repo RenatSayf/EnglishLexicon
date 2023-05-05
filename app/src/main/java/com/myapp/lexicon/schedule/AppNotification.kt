ee0e2a7453f54a26f0e40323bc52b3455467061c
Renat <renatsayf@gmail.com>
2023-05-05 22:46:25 +0500

    AppNotification.kt has been updated

--- app/src/main/java/com/myapp/lexicon/schedule/AppNotification.kt
+++ app/src/main/java/com/myapp/lexicon/schedule/AppNotification.kt
@@ -10,6 +10,7 @@ import android.content.SharedPreferences
 import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
 import com.myapp.lexicon.R
 import com.myapp.lexicon.helpers.StringOperations
@@ -42,8 +43,8 @@ class AppNotification constructor(private val context: Context) : Notification()
 
         notification = NotificationCompat.Builder(context, CHANNEL_ID).apply {
             setOngoing(false)
-            setSmallIcon(R.mipmap.ic_notification)
-            //color = ContextCompat.getColor(context, R.color.colorGreen)
+            setSmallIcon(R.drawable.icon_notification)
+            color = ContextCompat.getColor(context, R.color.colorAccent)
             setLargeIcon(BitmapFactory.decodeResource(context.resources, R.mipmap.ic_launcher))
             setAutoCancel(true)
             priority = NotificationCompat.PRIORITY_HIGH
