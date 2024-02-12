accc9c480583f81739525ddeb889dc3a92a8f029
Renat <renatsayf@gmail.com>
2024-02-12 12:31:15 +0500

    after merge

--- app/src/main/java/com/myapp/lexicon/repository/network/INetRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.myapp.lexicon.repository.network
-
-import com.myapp.lexicon.video.models.VideoSearchResult
-import kotlinx.coroutines.Deferred
-
-interface INetRepository {
-    suspend fun getSearchResult(searchString: String): Deferred<VideoSearchResult?>
-}
