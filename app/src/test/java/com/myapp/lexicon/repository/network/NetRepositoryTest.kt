e199ccd084f888bbacbb6ae71bfed2a7a16a65aa
RenatSayf <renatsayf@gmail.com>
2024-12-23 18:13:58 +0500

    Android Studio has been updated

--- app/src/test/java/com/myapp/lexicon/repository/network/NetRepositoryTest.kt
+++ app/src/test/java/com/myapp/lexicon/repository/network/NetRepositoryTest.kt
@@ -16,13 +16,6 @@ import org.junit.Test
 
 class NetRepositoryTest {
 
-    private val testSuggestionsResponse = """window.google.ac.h(["дру",[["друг вокруг",0,[512,433,131]],["другой мир",0,[512,433,131]],
-        |["другой взгляд",0,[512,650,433,131]],["другой мужчина",0,[512,650,433,131]],["друзья",0,[512,433]],
-        |["дружба это",0,[512]],["дружба",0,[512,433]],["другой мужчина сериал",0,[512,650,433,131]],
-        |["друзья сериал",0,[512,433]],["другая женщина",0,[512,433,131]],
-        |["другой мужчина смотреть онлайн 2023",0,[512,650,433,131]],["другие фильм",0,[512,433,131]],
-        |["друбич",0,[512,433,131]],["друг вокруг скачать",0,[512,433,131]]],
-        |{"k":1,"q":"58rBWO2jxrS0e8Hdk5jSH6gXYc4"}])""".trimMargin()
 
     private lateinit var repository: NetRepository
     private lateinit var mockEngine: MockEngine
@@ -41,58 +34,6 @@ class NetRepositoryTest {
     fun after() {
     }
 
-    @Test
-    fun decodeSearchResult() {
-        val result = Json.decodeFromString<VideoSearchResult>(TEST_VIDEO_LIST)
-        Assert.assertTrue(result.videoItems.isNotEmpty())
-    }
 
-    @Test
-    fun decodeSuggestionsFromString() {
-        val actualList = repository.decodeSuggestionsFromString(testSuggestionsResponse)
-        val actualSize = actualList.size
-        Assert.assertEquals(14, actualSize)
-    }
 
-    @Test
-    fun fetchSuggestions_success() {
-        runBlocking {
-            mockEngine = MockEngine(handler = {
-                respond(content = testSuggestionsResponse, status = HttpStatusCode.OK)
-            })
-            repository = NetRepository(HttpClient(mockEngine))
-
-            val result = repository.fetchSuggestions("xxx", "ru", userAgent = "USER_AGENT").await()
-            result.onSuccess { value: List<String> ->
-                Assert.assertEquals(14, value.size)
-            }
-            result.onFailure { exception ->
-                exception.printStackTrace()
-                Assert.assertTrue(exception.message, false)
-            }
-        }
-    }
-
-    @Test
-    fun getSearchResult_success() {
-        runBlocking {
-            mockEngine = MockEngine(handler = { requestData ->
-                if (requestData.url.parameters.contains("q", "friends"))
-                    respond(content = TEST_VIDEO_LIST, status = HttpStatusCode.OK)
-                else
-                    respond(content = "")
-            })
-
-            repository = NetRepository(HttpClient(mockEngine))
-
-            val actualResult = repository.getSearchResult(query = "friends", pageToken = "", maxResults = 5).await()
-            actualResult.onSuccess { value: VideoSearchResult ->
-                Assert.assertTrue(value.videoItems.isNotEmpty())
-            }
-            actualResult.onFailure { exception ->
-                exception.printStackTrace()
-                Assert.assertTrue(exception.message, false)
-            }
-        }
-    }
 }
