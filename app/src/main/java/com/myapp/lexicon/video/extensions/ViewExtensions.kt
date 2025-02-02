38e58089fbb447bea9bfc974c1b2289382835b67
RenatSayf <renatsayf@gmail.com>
2025-02-02 17:46:15 +0500

    ELM-11 in progress

--- app/src/main/java/com/myapp/lexicon/video/extensions/ViewExtensions.kt
+++ app/src/main/java/com/myapp/lexicon/video/extensions/ViewExtensions.kt
@@ -4,62 +4,9 @@ package com.myapp.lexicon.video.extensions
 
 import android.animation.Animator
 import android.animation.ValueAnimator
-import android.app.SearchManager
-import android.content.Context
-import android.database.Cursor
-import android.database.MatrixCursor
-import android.provider.BaseColumns
 import androidx.appcompat.widget.LinearLayoutCompat
-import androidx.appcompat.widget.SearchView
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.cursoradapter.widget.CursorAdapter
-import androidx.cursoradapter.widget.SimpleCursorAdapter
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
-
-fun SearchView.createAdapter(): SimpleCursorAdapter {
-    return SimpleCursorAdapter(
-        this.context,
-        android.R.layout.simple_list_item_1,
-        null,
-        arrayOf(SearchManager.SUGGEST_COLUMN_TEXT_1),
-        intArrayOf(android.R.id.text1),
-        CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
-    )
-}
-
-fun SearchView.updateAdapter(list: List<String>){
-    val cursor = MatrixCursor(arrayOf(BaseColumns._ID, SearchManager.SUGGEST_COLUMN_TEXT_1))
-    list.forEachIndexed { index, text ->
-        cursor.addRow(arrayOf(index, text))
-    }
-    (this.suggestionsAdapter as SimpleCursorAdapter).changeCursor(cursor)
-}
-
-fun SearchView.getSelectedSuggestion(
-    position: Int,
-    onSelection: (selection: String) -> Unit
-) {
-    val cursor = this.suggestionsAdapter.getItem(position) as Cursor
-    val columnIndex = cursor.getColumnIndex(SearchManager.SUGGEST_COLUMN_TEXT_1)
-    if (columnIndex >= 0) {
-        val selection = cursor.getString(columnIndex)
-        this.setQuery(selection, false)
-        onSelection.invoke(selection)
-    }
-}
-
-
-private var playerInstance: YouTubePlayerView? = null
-fun Context.youTubePlayerView(): YouTubePlayerView {
-    if (playerInstance == null) {
-        playerInstance = YouTubePlayerView(this)
-    }
-    return playerInstance!!.apply {
-        enableAutomaticInitialization = false
-
-    }
-}
 
 fun LinearLayoutCompat.changeHeightAnimatedly(
     value: Int = this.height,
--- app/src/main/java/com/myapp/lexicon/video/models/query/SearchQuery.kt
+++
@@ -1,5 +0,0 @@
-package com.myapp.lexicon.video.models.query
-
-data class SearchQuery(
-    override val text: String
-): ISearchItem
